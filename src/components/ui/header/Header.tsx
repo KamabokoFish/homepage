7fe178c75b82b530bfb58138bfb4e4c940070f6c
kamaboko <79447012+KamabokoFish@users.noreply.github.com>
2024-11-15 01:25:35 +0900

    refactor(header): adjust top position

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -4,7 +4,7 @@ import BreadCrumbNav from './BreadCrumbNav';
 
 const Header: React.FC = () => {
   return (
-    <header className="mx-auto px-6 w-11/12 max-w-3xl sticky top-12 bg-whiteTrans  rounded-full shadow-xl backdrop-blur-md border border-white/20 z-50 text-white">
+    <header className="mx-auto px-6 w-11/12 max-w-3xl sticky top-10 bg-whiteTrans  rounded-full shadow-xl backdrop-blur-md border border-white/20 z-50 text-white">
       <div className="flex justify-between items-center h-14 w-full">
         <h1 className="font-bold">
           <Link href={'/'}>KAMABOKO</Link>
--- src/components/ui/utils/ContentWrapper.tsx
+++ src/components/ui/utils/ContentWrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 //コンテンツ全体の幅を制御するコンテナ(rootのlayout.tsxで利用)
 const ContentWrapper: React.FC<Props> = ({ children }) => {
-  return <main className="mx-auto mt-24 px-6 w-11/12 max-w-3xl">{children}</main>;
+  return <main className="mx-auto mt-20 px-6 w-11/12 max-w-3xl min-h-[550px]">{children}</main>;
 };
 
 export default ContentWrapper;
